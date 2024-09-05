0602455a76f6f879f4d5c5868712698620c88f7d
vmontezano <vmontezano@gmail.com>
2024-09-05 07:54:41 -0300

    Salvando o projeto pela primeira vez

---
+++ dados.js
@@ -0,0 +1,43 @@
+let filmes = [
+    {
+        titulo : "Matrix",
+        sinopse : "Matrix um clássico da ficção científica que nos convida a questionar a natureza da realidade. Em um futuro distópico, a humanidade é prisioneira de uma simulação virtual complexa, chamada Matrix. Criada por máquinas inteligentes, essa realidade simulada mantém os humanos inconscientes, enquanto seus corpos são usados como fonte de energia.",
+        link : "https://www.imdb.com/title/tt0133093/"
+    },
+    {
+        titulo : 'Batman (1989)',
+        sinopse : 'O clássico que redefiniu o gênero de super-heróis, apresentando um Batman sombrio e realista, interpretado por Michael Keaton.',
+        link: 'https://www.imdb.com/title/tt0096895/'
+    },
+    {
+        titulo : 'À Espera de um Milagre',
+        sinopse : 'Um drama emocionante ambientado em uma instituição mental, que explora temas como esperança, redenção e a natureza da insanidade.',
+        link : 'https://www.imdb.com/title/tt0120689/'
+    },
+    {
+        titulo : 'Seven',
+        sinopse : 'Um thriller psicológico perturbador sobre dois detetives que investigam uma série de assassinatos inspirados nos sete pecados capitais.',
+        link : 'https://www.imdb.com/title/tt0114369/'
+     },
+     {
+        titulo : "Um Estranho no Ninho",
+        sinopse : "Um drama psicológico que explora a luta de um homem contra um sistema opressivo em um hospital psiquiátrico. O filme aborda temas como individualidade, conformismo e a importância da liberdade.",
+        link : "https://www.imdb.com/title/tt0073784/"
+   },
+   {
+        titulo : "O Exorcista",
+        sinopse : "Um clássico do terror que narra a possessão demoníaca de uma jovem garota. O filme aborda temas como fé, religião e o mal.",
+        link : "https://www.imdb.com/title/tt0073312/"
+   },
+   {
+     titulo : "Todos os Homens do Presidente",
+     sinopse : "Um drama baseado em fatos reais que retrata a investigação do jornal The Washington Post sobre o escândalo de Watergate. O filme explora a importância do jornalismo investigativo e a liberdade de imprensa.",
+     link : "https://www.imdb.com/title/tt0071316/"
+   },
+   {
+     titulo : "Dirty Harry",
+     sinopse : "Um filme de ação que acompanha as aventuras do inspetor Harry Callahan, um policial durão e controverso que usa métodos pouco ortodoxos para combater o crime. O filme aborda temas como justiça, moralidade e o sistema legal.",
+     link : "https://www.imdb.com/title/tt0068445/"
+   }
+];
+
